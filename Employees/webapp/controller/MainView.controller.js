581b231fc88252dcf0376d370ec5ea19ce2277a6
cazz7 <cazz182@gmail.com>
2021-04-16 03:00:56 +0000

    Employees

--- Employees/webapp/controller/MainView.controller.js
+++ Employees/webapp/controller/MainView.controller.js
@@ -1,26 +1,61 @@
 sap.ui.define([
-		"sap/ui/core/mvc/Controller"
-	],
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+],
 	/**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
+     * @param {typeof sap.ui.model.json.JSONModel} JSONModel
      */
-	function (Controller) {
-        "use strict";    
-        
-        // Se implementan la lógica de otra forma para evitar que marque errores
-		const Main = Controller.extend("logaligroup.Employees.controller.MainView", {}); 
-            
-        Main.prototype.onValidate = function() {
-                var inputEmployee = this.byId("inputEmployee");
-                var valueEmployee = inputEmployee.getValue();
+    function (Controller, JSONModel) {
+        "use strict";
 
-                if(valueEmployee.length === 6){
-                    this.getView().byId("labelCountry").setVisible(true);
-                    this.getView().byId("slCountry").setVisible(true);
-                }else{
-                    this.getView().byId("labelCountry").setVisible(false);
-                    this.getView().byId("slCountry").setVisible(false);
-                }
+        function onInit() {
+
+            var oJSONModel = new JSONModel();
+            var oView = this.getView();
+            var i18nBundle = oView.getModel("i18n").getResourceBundle();
+
+            var oJSON = {
+                employeeId: "12345",
+                countryKey: "UK",
+                listCountry: [
+                    {
+                        key: "US",
+                        text: i18nBundle.getText("countryUS")
+                    },
+                    {
+                        key: "UK",
+                        text: i18nBundle.getText("countryUK")
+                    },
+                    {
+                        key: "ES",
+                        text: i18nBundle.getText("countryES")
+                    },
+                ]
+
             };
+
+            oJSONModel.setData(oJSON);
+            oView.setModel(oJSONModel);
+        };
+
+        // Se implementan la lógica de otra forma para evitar que marque errores
+        const Main = Controller.extend("logaligroup.Employees.controller.MainView", {});
+
+        Main.prototype.onValidate = function () {
+            var inputEmployee = this.byId("inputEmployee");
+            var valueEmployee = inputEmployee.getValue();
+
+            if (valueEmployee.length === 6) {
+                this.getView().byId("labelCountry").setVisible(true);
+                this.getView().byId("slCountry").setVisible(true);
+            } else {
+                this.getView().byId("labelCountry").setVisible(false);
+                this.getView().byId("slCountry").setVisible(false);
+            }
+        };
+
+        Main.prototype.onInit = onInit;
+
         return Main;
-	});
+    });
