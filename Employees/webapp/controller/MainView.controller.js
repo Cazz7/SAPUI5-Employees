f1f1b68ad4a5661c2eaa50d924e77222a35260f1
cazz7 <cazz182@gmail.com>
2021-04-23 02:57:27 +0000

    Se accede al contexto y luego a propiedades del json desde este

--- Employees/webapp/controller/MainView.controller.js
+++ Employees/webapp/controller/MainView.controller.js
@@ -72,7 +72,15 @@ sap.ui.define([
             var oModel = this.getView().getModel();
             oModel.setProperty("/EmployeeId", "");
             oModel.setProperty("/CountryKey", "");
-        };        
+        };
+        
+        function showPostalCode(oEvent){            
+            var itemPressed = oEvent.getSource();
+            var oContext = itemPressed.getBindingContext();
+            var objectContext = oContext.getObject();
+
+            sap.m.MessageToast.show(objectContext.PostalCode);
+        };         
 
         // Se implementan la lógica de otra forma para evitar que marque errores
         const Main = Controller.extend("logaligroup.Employees.controller.MainView", {});
@@ -93,6 +101,7 @@ sap.ui.define([
         Main.prototype.onInit = onInit;
         Main.prototype.onFilter = onFilter;
         Main.prototype.onClearFilter = onClearFilter;
+        Main.prototype.showPostalCode = showPostalCode;
 
         return Main;
     });
